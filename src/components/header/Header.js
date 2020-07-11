836e5e35b78a54d3eeb82ef8545485b102d81352
Denis Isaev <denis.isaev@rt.ru>
2020-07-11 23:04:09 +0300

    feature | add rendering HTML template method

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,7 @@
 import { ExcelComponent } from '@core'
 
 export default class Header extends ExcelComponent {
-
+  toHTML() {
+    return '<h1>Header</h1>'
+  }
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,7 @@
 import { ExcelComponent } from '@core'
 
 export default class Table extends ExcelComponent {
-
+  toHTML() {
+    return '<h1>Table</h1>'
+  }
 }
