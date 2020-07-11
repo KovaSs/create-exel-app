312ee6b0bf195471019f684917010eefc0de0208
Denis Isaev <denis.isaev@rt.ru>
2020-07-12 00:39:04 +0300

    feature | add class to working with DOM

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,7 +2,7 @@ import { $ } from '@core'
 
 export default class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
@@ -12,10 +12,9 @@ export default class Excel {
     this.components.forEach((Component) => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     });
-
     return $root
   }
 
