77f2c751c02f284ff8df8d784fedc904ab38f228
Isaev Denis <sr_relikt@mail.ru>
2020-07-12 14:24:49 +0300

    chore | add listeners to resize table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,47 @@ import createTable from './table.template'
 export default class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: [
+        'click',
+        'mousedown',
+        'mouseup',
+        'mousemove',
+      ]
+    })
+  }
+
   toHTML() {
     return createTable(20)
+  }
+
+  onMousedown(e) {
+    console.debug(
+        `${this.name}: onMousedown`,
+        this.$root,
+        e.target
+    )
+  }
+
+  onMouseup(e) {
+    console.debug(
+        `${this.name}: onMouseup`,
+        this.$root,
+        e.target
+    )
+  }
+
+  onMousemove(e) {
+    console.debug(
+        `${this.name}: onMousemove`,
+        this.$root,
+        e.target
+    )
+  }
+
+  onClick(e) {
+    console.debug(`${this.name}: onClick`, this.$root, e.target)
   }
 }
